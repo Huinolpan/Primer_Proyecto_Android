971d3444277e2f7d3a3ccca739e765b63bdae211
Eric Romero <eric.romero@cchen.cl>
2021-11-18 12:24:54 -0300

    Agregué más palabras al mensaje de inicio de sesión.

--- app/src/main/java/com/example/proyecto_android_normahuinolpancid/MainActivity.java
+++ app/src/main/java/com/example/proyecto_android_normahuinolpancid/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity
             case "Norma":
                 if(elkatuchiNemul.equals("123"))
                 {
-                    werkun.setText("Ingreso  exitoso");
+                    werkun.setText("Ingreso super exitoso");
 
                     Intent i = new Intent(this,servicios_proyecto.class);
                     startActivity(i);
